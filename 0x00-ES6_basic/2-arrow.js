ef8585fb120e7e528879125bea9e8b1d6d61a259
Mariem <mariem.ehab234@gmail.com>
2024-05-27 13:34:16 +0000

    Task 2

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,11 +1,9 @@
-const getNeighborhoodsList = () => {
-  const sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
   this.addNeighborhood = (newNeighborhood) => {
-    [...sanFranciscoNeighborhoods].push(newNeighborhood);
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;
   };
 }
-
-export default getNeighborhoodsList;
