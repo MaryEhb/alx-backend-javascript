d53ac24b41660ed69ccc9e442563894b14ab2584
Mariem <mariem.ehab234@gmail.com>
2024-05-31 10:18:12 +0000

    Task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,8 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([res1, res2]) => {
+      console.log(`${res1.body} ${res2.firstName} ${res2.lastName}`);
+    }).catch(() => Error('Signup system offline'));
+}
