718a2a44d5993f8433fa280eaeec5718d3d82d94
Mariem <mariem.ehab234@gmail.com>
2024-05-31 10:51:13 +0000

    Task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,11 +3,8 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, firstName), uploadPhoto(fileName)])
-    .then(([res1, res2]) => {
-      return [
-        { status: res1.status, value: res1.value },
-	{ status: res2.status, value: res2.reason }
-      ];
-    }
-  );
+    .then(([res1, res2]) => [
+      { status: res1.status, value: res1.value },
+      { status: res2.status, value: res2.reason },
+    ]);
 }
