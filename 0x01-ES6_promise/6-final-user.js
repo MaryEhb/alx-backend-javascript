a60f1f18a589cd7dcfdeb351eda1fd2b4b7294be
Mariem <mariem.ehab234@gmail.com>
2024-05-31 12:24:57 +0000

    Task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,6 +5,6 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, firstName), uploadPhoto(fileName)])
     .then(([res1, res2]) => [
       { status: res1.status, value: res1.status === 'fulfilled' ? res1.value : res1.reason },
-      { status: res2.status, value: res2.status === 'fulfilled' ? res2.value : res2.reason },
+      { status: res2.status, value: res2.status === 'fulfilled' ? res2.value : String(res2.reason) },
     ]);
 }
