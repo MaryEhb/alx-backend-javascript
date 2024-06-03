263f4c633cade2e16f7b91a4683e962ed97c5a53
Mariem <mariem.ehab234@gmail.com>
2024-06-03 20:45:22 +0000

    Task 7

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,3 +1,8 @@
-export default function hasValuesFromArray(s, a) {
-  return s.has(...a);
+export default function hasValuesFromArray(set, array) {
+  for (const item of array) {
+    if (!set.has(item)) {
+      return false;
+    }
+  }
+  return true;
 }
