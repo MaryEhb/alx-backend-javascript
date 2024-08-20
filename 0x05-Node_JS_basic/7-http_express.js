e3b14ee4ff4ecac37fd0e29c4bda5ebe150414f0
Mariem <mariem.ehab234@gmail.com>
2024-08-20 08:21:37 -0700

    Task 7

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const fs = require('fs');
+
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+
+    let fileLines = data.trim().split('\n');
+    fileLines = fileLines.filter((line) => line.trim() !== '');
+
+    const fieldsDetails = {};
+    for (let i = 1; i < fileLines.length; i += 1) {
+      const values = fileLines[i].split(',');
+      const field = values[values.length - 1].trim();
+      const name = values[0].trim();
+
+      if (field in fieldsDetails) {
+        fieldsDetails[field].push(name);
+      } else {
+        fieldsDetails[field] = [name];
+      }
+    }
+    let response = '';
+    response += (`Number of students: ${fileLines.length - 1}`);
+    for (const [field, names] of Object.entries(fieldsDetails)) {
+      const list = names.join(', ');
+      response += (`\nNumber of students in ${field}: ${names.length}. List: ${list}`);
+    }
+
+    resolve(response);
+  });
+});
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.type('text/plain');
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
+  res.type('text/plain');
+  countStudents(DB_FILE).then((promiseRes) => {
+    res.send(`This is the list of our students\n${promiseRes}`);
+  }).catch((error) => {
+    res.send(error.message);
+  });
+});
+
+app.listen(1245);
+
+module.exports = app;
