9b8b8e98a2d78983e9a5f100249b1e2ae3faa33a
Mariem <mariem.ehab234@gmail.com>
2024-08-20 06:04:00 -0700

    Task 5

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,58 @@
+const http = require('http');
+const fs = require('fs');
+
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+
+    let fileLines = data.trim().split('\n');
+    fileLines = fileLines.filter((line) => line.trim() !== '');
+
+    const fieldsDetails = {};
+    for (let i = 1; i < fileLines.length; i += 1) {
+      const values = fileLines[i].split(',');
+      const field = values[values.length - 1].trim();
+      const name = values[0].trim();
+
+      if (field in fieldsDetails) {
+        fieldsDetails[field].push(name);
+      } else {
+        fieldsDetails[field] = [name];
+      }
+    }
+    let response = '';
+    response += (`Number of students: ${fileLines.length - 1}`);
+    for (const [field, names] of Object.entries(fieldsDetails)) {
+      const list = names.join(', ');
+      response += (`\nNumber of students in ${field}: ${names.length}. List: ${list}`);
+    }
+
+    resolve(response);
+  });
+});
+
+const app = http.createServer((request, response) => {
+  if (request.url === '/students') {
+    const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
+    response.write('This is the list of our students\n');
+    countStudents(DB_FILE).then((promiseRes) => {
+      response.write(promiseRes);
+      response.end();
+    }).catch((error) => {
+      response.write(error.join('\n'));
+      response.end();
+    });
+  } else if (request.url === '/') {
+    response.write('Hello Holberton School!');
+    response.end();
+  } else {
+    response.end();
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
