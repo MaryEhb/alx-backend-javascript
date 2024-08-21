ba94af7df9fe612c7cced1d28ceda8053f54fb21
Mariem <mariem.ehab@ieee.org>
2024-08-21 17:07:13 +0000

    Task 4 rm spaces

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -8,10 +8,9 @@ describe('sendPaymentRequestToApi', () => {
   it('Test 0', () => {
     const spy = sinon.spy(console, 'log');
     const stub = sinon.stub(Utils, 'calculateNumber');
+
     stub.returns(10);
-
     sendPaymentRequestToApi(100, 20);
-
     expect(stub.calledWith('SUM', 100, 20)).to.be.true;
     expect(stub.callCount).to.be.equal(1);
     expect(spy.calledWith('The total is: 10')).to.be.true;
