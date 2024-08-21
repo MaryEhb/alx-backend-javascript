40fabe2c7fce8dab194d89c1c6d4a3ecd00d4ad6
Mariem <mariem.ehab@ieee.org>
2024-08-21 17:25:40 +0000

    Task 5

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,1 +1,21 @@
-const
+const sendPaymentRequestToAPI = require('./5-payment');
+const sinon = require('sinon');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('sendPaymentRequestToAPI', () => {
+  let spy;
+  beforeEach(() => {
+    spy = sinon.spy(console, 'log');
+  });
+
+  it('Test 0', () => {
+    sendPaymentRequestToAPI(100, 20);
+    expect(spy.calledWith('The total is: 120')).to.be.true;
+    expect(spy.calledOnce).to.be.true;
+  });
+
+  afterEach(() => {
+    spy.restore();
+  });
+});
